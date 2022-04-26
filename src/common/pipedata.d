8c87aa2616e1bd0cb3eb1369e969d97b6c0ad34d
human <human@neet.fi>
2022-04-27 00:59:01 +0300

    fix align() syntax
    meant to apply it to the struct fields, not the struct itself

--- src/common/pipedata.d
+++ src/common/pipedata.d
@@ -1,18 +1,16 @@
 module ddw.common.pipedata;
 
-align(1)
 struct processing_request
 {
+align(1):
 	ulong buffer_size; /* how many bytes are written after this header */
 	uint samplerate;
 	ubyte bitspersample;
 	ubyte channels;
 }
-static assert(processing_request.sizeof == (64+32+8+8)/8);
 
-align(1)
 struct processing_response
 {
+align(1):
 	ulong buffer_size; /* how many bytes are written after this header */
 }
-static assert(processing_response.sizeof == (64)/8);
--- src/common/shmdata.d
+++ src/common/shmdata.d
@@ -5,9 +5,9 @@ enum ISPLAYING_PAUSED = 3;
 enum ISPLAYING_NOTPLAYING = 2;
 
 // https://www.geoffchappell.com/studies/windows/km/ntoskrnl/inc/api/ntexapi_x/kuser_shared_data/index.htm
-align(1)
 struct Shm
 {
+align(1):
 	int playback_pos_ms;
 
 	char[512] track_title = 0;
